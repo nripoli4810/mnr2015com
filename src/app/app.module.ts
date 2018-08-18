320a4a424dd724c5840a9ad7c9b4a2443163ad9f
Nate Ripoli <ripoline@gcc.edu>
2018-08-18 14:11:07 -0400

    Fix navigation side container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { TileComponent } from './tile/tile.component';
 
-import { MatCardModule, MatToolbarModule, MatGridListModule, MatSidenavModule } from '@angular/material';
-import { SideNavigationComponent } from './side-navigation/side-navigation.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { NavigationComponent } from './navigation/navigation.component';
+import { MatCardModule, MatToolbarModule, MatGridListModule, MatSidenavModule, MatListModule, MatIcon, MatIconModule } from '@angular/material';
 
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TileComponent,
-    SideNavigationComponent,
-    ToolbarComponent,
     NavigationComponent
   ],
   imports: [
@@ -25,7 +21,9 @@ import { NavigationComponent } from './navigation/navigation.component';
     MatToolbarModule,
     MatGridListModule,
     MatSidenavModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
-  isOpen: boolean;
+  opened: boolean;
 
   constructor() { }
 
@@ -15,6 +15,8 @@ export class NavigationComponent implements OnInit {
 
   toggle() {
     console.log("toggle called");
+    this.opened = !this.opened;
+
     // this.isNavVisible = !this.isNavVisible;
     // if (this.isNavVisible) {
     //   this.open.emit(null);
