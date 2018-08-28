f2190073699b2fdf975f378cbeea65052ba13bd7
Nate Ripoli <ripoline@gcc.edu>
2018-08-28 15:09:04 -0400

    Updated routing module to use landing page instead of app component for home link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule, InjectionToken } from '@angular/core';
 import { Routes, RouterModule, ActivatedRouteSnapshot } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { GalleriesComponent } from './galleries/galleries.component';
 import { BabyComponent } from './baby/baby.component';
 import { NotFoundComponent } from './not-found.component';
+import { LandingComponent } from './landing/landing.component';
 
 const externalUrlRedirectResolver = new InjectionToken('externalUrlRedirectResolver');
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: AppComponent },
+  { path: 'home', component: LandingComponent },
   { path: 'galleries', component: GalleriesComponent },
   { path: 'baby', component: BabyComponent },
   {
