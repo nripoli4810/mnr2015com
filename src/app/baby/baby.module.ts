16fbfd22a106541acff30b7a82df2b007d71f9b3
Nate Ripoli <ripoline@gcc.edu>
2018-08-30 19:28:57 -0400

    Changed Baby page from material grid to pure html with flex layout

--- src/app/baby/baby.module.ts
+++ src/app/baby/baby.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BabyComponent } from './baby.component';
 import { MatGridListModule, MatListModule } from '@angular/material';
 import { SharedComponentsModule } from '../shared/shared.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     MatGridListModule, MatListModule,
-    SharedComponentsModule
+    SharedComponentsModule,
+    FlexLayoutModule
   ],
   declarations: [BabyComponent],
   exports: [BabyComponent]
