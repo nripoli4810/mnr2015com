3c2e80f538d31465cb29d3988095b62cba7e8938
Margaret Ripoli <mripoli@mbpro.local>
2018-09-04 20:01:12 -0400

    Update registry and shower pages

--- src/app/mock/shower.ts
+++ src/app/mock/shower.ts
@@ -49,7 +49,7 @@ export const MockShowers: Shower[] = [
     {
         id: 3,
         title: 'Cleveland, OH',
-        date: 'Sunday November 11',
+        date: 'TBD',
         time: '1:00 PM',
         content: 'Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow\'s nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters. \
         Deadlights jack lad schooner scallywag dance the hempen jig carouser broadside cable strike colors. Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to. \
