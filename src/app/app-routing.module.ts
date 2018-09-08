b1c43e531ed5d81a4ad134f086812540a29d9668
Nate Ripoli <ripoline@gcc.edu>
2018-09-08 09:36:49 -0400

    Use Hash Routing to fix refresh 404 issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { useHash: true }),
   ],
   exports: [
     RouterModule
